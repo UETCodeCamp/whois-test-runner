5f74d231a5c5c4e7514983e07b7f04c8835b02e2
Minh Nguyen <tienminh.uet@gmail.com>
2019-04-14 20:12:21 +0700

    runner 02

--- images/runner-02/student-runner/index.js
+++ images/runner-02/student-runner/index.js
@@ -7,7 +7,7 @@ async function start() {
 	try {
 		const text = await compiler.compile(process.env.STUDENT_REPO_PATH)
 		const ot = fs.createWriteStream(process.env.STUDENT_STD_OUT_FILE)
-		ot.write(text)
+		ot.write(text, 'utf8')
 	} catch (err){
 		console.log(err)
 	} 
